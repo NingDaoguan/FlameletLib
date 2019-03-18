f07d78d46668add4aeb14128f49e4bf49f310bc3
ZX114 <xu-zhang@sjtu.edu.cn>
2019-03-18 20:59:48 +0800

    Create scatter plot for T in Z-chi or Z-Yc space

--- test/plot.py
+++ test/plot.py
@@ -17,11 +17,8 @@ plt.figure(figsize=figs)
 for filename1 in filename:
     data1 = np.loadtxt(filename1,delimiter=',',skiprows=1)
     data1 = np.transpose(data1)
-    #f.grid
     grid1 = data1[0]
-    #f.T
     T1 = data1[1]
-    #plot T
     plt.plot(grid1,T1,label = fr'${filename1}$',lw=linew)
     print(filename1)
     a = (T1[0] - T1[-1]) / (grid1[-1]-grid1[0])
@@ -31,7 +28,6 @@ plt.title('Velocity Field',fontsize=fonts1)
 plt.xlabel(r'x (m)',fontsize=fonts1)
 plt.ylabel(r'u (m/s)',fontsize=fonts1)
 plt.legend(loc=0,fontsize=fonts1)
-# plt.savefig('VelocityField.png', dpi=500)
 
 
 plt.figure(figsize=figs)
@@ -44,27 +40,4 @@ plt.tick_params(labelsize=fonts2)
 plt.xlabel(r'x (m)',fontsize=fonts2)
 plt.ylabel(r'T (K)',fontsize=fonts2)
 
-
-
-# plt.figure(figsize=figs)
-# for ic,filename1 in enumerate(filename):
-#     data1 = np.loadtxt(filename1,delimiter=',',skiprows=1)
-#     data1 = np.transpose(data1)
-#     # f.grid
-#     grid1 = data1[0]
-#     # YAR
-#     YAR = data1[6]
-#     YARO = YAR[-1]
-
-#     Z1 = (YAR - YARO) / (0.0-YARO)
-#     plt.plot(grid1,Z1,label = fr'$Z$ @ ${filename1}$',c='k',ls=lss[ic],lw=linew)
-# plt.tick_params(labelsize=fonts1)
-# # plt.title('Mixture Fraction',fontsize=fonts1)
-# plt.xticks(x_ticks,color='k')
-# plt.xlabel(r'x (m)',fontsize=fonts1)
-# plt.ylabel(r'Z (-)',fontsize=fonts1)
-# plt.xlim(0,0.02)
-# # plt.ylim(0,1)
-# plt.legend(loc=0,fontsize=fonts1)
-# plt.savefig('MixtureFraction.png', dpi=500)
 plt.show()
--- utilities/scatterPlot.py
+++ utilities/scatterPlot.py
@@ -6,9 +6,10 @@ import matplotlib
 x = input("Enter loop number:\n")
 x = int(x)
 y = input("Yc(0) or chi(1)?\n")
-filename = []
-for n in range(x):
-    filename.append('flameletTable_{:}.csv'.format(n))
+#filename = []
+#for n in range(x+1):
+#    filename.append('flameletTable_{:}.csv'.format(n))
+filename = ['lookupResult.csv']
 
 fonts1 = 23
 fonts2 = 25
@@ -20,26 +21,26 @@ plt.rcParams['font.family'] = "Serif"
 for ic,filename1 in enumerate(filename):
     data1 = np.loadtxt(filename1,delimiter=',',skiprows=1)
     data1 = np.transpose(data1)
-    grid1 = data1[0]
     YAR = data1[3]
     YARO = YAR[0]
-    Z1 = (YAR - YARO) / (0.0-YARO)
+    Z1 = data1[0]
     Yc = data1[1]
     norm = matplotlib.colors.Normalize(vmin=300, vmax=2300)
-    sc = plt.scatter(Z1,Yc,c=data1[2],cmap=plt.cm.rainbow,s=10,norm=norm)
+    sc = plt.scatter(Z1,Yc,c=data1[2],cmap=plt.cm.rainbow,s=2,norm=norm)
 
 v = [300,500,1000,1500,2000]
 cbar = plt.colorbar(sc,ticks=v)
-cbar.set_label('T (K)', fontsize=fonts2)
+cbar.set_label(r'T (K)', fontsize=fonts2)
 for t in cbar.ax.get_yticklabels():
     t.set_fontsize(fonts2)
 plt.tick_params(labelsize=fonts2)
 plt.xlabel(r'Z (-)',fontsize=fonts2)
 if y == '0':
-    plt.ylabel(r'Yc (-)',fontsize=fonts2)
+    plt.ylabel(r'$Y_c$ (-)',fontsize=fonts2)
 elif y == '1':
-    plt.ylabel(r'chi (-)',fontsize=fonts2)
+    plt.ylabel(r'$\chi$ (-)',fontsize=fonts2)
 else:
     print("Input Error")
 
+plt.savefig('SLF_Z'+y+'.png',dpi=500)
 plt.show()
