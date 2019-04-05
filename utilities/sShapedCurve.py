96c2508ae1bcac4f869d6b04c922b8000e8e101c
ZX114 <xu-zhang@sjtu.edu.cn>
2019-04-05 11:14:22 +0800

    Generalize utilities

---
+++ utilities/sShapedCurve.py
@@ -0,0 +1,50 @@
+# Wed Mar 20 09:02:17 CST 2019
+import numpy as np
+import matplotlib.pyplot as plt
+figs = (13,10)
+fonts1 = 23
+fonts2 = 25
+linew = 3
+plt.rcParams['font.family'] = 'serif'
+from matplotlib import rc
+plt.rcParams['mathtext.fontset'] = 'stix'
+# x_ticks = np.arange(0.0, 0.025, 0.005)
+
+Zst = 0.0633883
+numLoop = int(input('Enter loop number:\n')) # Total number of loops
+nLoop = range(0,numLoop+1,1)
+chiSt = []
+Tmax = []
+for n in nLoop:
+    filename = 'flameletTable_{:}.csv'.format(n)
+
+    data = np.loadtxt(filename, delimiter=',', skiprows=1)
+    data = np.transpose(data)
+    Z = data[0]
+    chi = data[1]
+    T = data[2]
+
+    for j,iZ in enumerate(Z):
+        if iZ > Zst:
+            Zub = iZ
+            Zlb = Z[j-1]
+            weightU = (Zst - Zlb) / (Zub - Zlb)
+            weightL = (Zub - Zst) / (Zub - Zlb)
+            pU = j
+            pL = j-1
+            break
+        else:
+            continue
+
+    chiSt.append(chi[pU] * weightU + chi[pL] * weightL)
+    Tmax.append(np.max(T))
+
+# Plot T-Z
+plt.figure(figsize=figs)
+plt.scatter(chiSt,Tmax,c='k',marker='o')
+# plt.scatter(np.log10(chiSt),Tmax,c='k',marker='o')
+plt.tick_params(labelsize=fonts1)
+plt.xlabel(r'$\chi_{st} \ (1/s)$',fontsize=fonts1)
+plt.ylabel(r'$T_{max} \ (K)$',fontsize=fonts1)
+
+plt.show()
---
+++ utilities/scatterPlot.py
@@ -0,0 +1,52 @@
+#Sat Dec 15 15:50:09 CST 2018
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+
+x = input("Enter loop number:\n")
+x = int(x)
+y = input("Yc(0) or chi(1)?\n")
+filename = []
+if x > 1:
+    for n in range(x+1):
+       filename.append('flameletTable_{:}.csv'.format(n))
+else:
+    filename = ['lookupResult.csv']
+
+fonts1 = 23
+fonts2 = 25
+linew = 3
+figs = (13,10)
+
+plt.figure(figsize=figs)
+plt.rcParams['font.family'] = "Serif"
+from matplotlib import rc
+plt.rcParams['mathtext.fontset'] = 'stix'
+for ic,filename1 in enumerate(filename):
+    data1 = np.loadtxt(filename1,delimiter=',',skiprows=1)
+    data1 = np.transpose(data1)
+    YAR = data1[3]
+    YARO = YAR[0]
+    Z1 = data1[0]
+    Yc = data1[1]
+    norm = matplotlib.colors.Normalize(vmin=300, vmax=2300)
+    sc = plt.scatter(Z1,Yc,c=data1[2],cmap=plt.cm.rainbow,s=2,norm=norm)
+
+v = [300,500,1000,1500,2000]
+cbar = plt.colorbar(sc,ticks=v)
+cbar.set_label(r'T (K)', fontsize=fonts2)
+for t in cbar.ax.get_yticklabels():
+    t.set_fontsize(fonts2)
+plt.xlim(0,1)
+plt.ylim(0,1200)
+plt.tick_params(labelsize=fonts2)
+plt.xlabel(r'Z (-)',fontsize=fonts2)
+if y == '0':
+    plt.ylabel(r'$Y_c \ (-)$',fontsize=fonts2)
+elif y == '1':
+    plt.ylabel(r'$\chi \ (-)$',fontsize=fonts2)
+else:
+    print("Input Error")
+
+# plt.savefig('SLF_Z'+y+'.png',dpi=500,bbox_inches='tight')
+plt.show()
